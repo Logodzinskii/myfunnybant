8b041f306d096b97d1a0ef84c191e7355d9b1e82
Aleksandr <Logodzinskii>
2022-07-28 17:43:52 +0500

    Добавлена возможность удалять занесенные расходы и привязка к геопозиции

--- botapi/YaDisk/tmebot/Classes/Report.php
+++ botapi/YaDisk/tmebot/Classes/Report.php
@@ -10,15 +10,40 @@ class Report
 
     public function addExpenses($arrExpenses)
     {
+        // https://github.com/hflabs/dadata-php
+        require_once $_SERVER['DOCUMENT_ROOT'].'/botapi/YaDisk/tmebot/Classes/Dadata.php';
+
+        $token = "b280cf1246433ce7eac426b191f1f8fe65f7eab0";
+        $dadata = new Dadata($token, null);
+        $dadata->init();
+
+        $str = $arrExpenses['location'];
+
+        $array = get_object_vars(json_decode($str));
+
+        if($array['latitude'] == 0 && $array['longitude'] == 0)
+        {
+
+            $result = $dadata->geolocate(56.819124, 60.631784);
+
+        }else{
+
+            $result = $dadata->geolocate($array['latitude'], $array['longitude']);
+
+        }
+
+
         $date = new DateTime('NOW');
         $today = $date->format("Y-m-d");
         try{
-            $stmt = $this->connection->prepare("INSERT INTO `expenses` SET `saller` =:saller, `name_expens` =:name_expens, `totalPrice`=:totalPrice, `date`=:date ");
+            $stmt = $this->connection->prepare("INSERT INTO `expenses` SET `saller` =:saller, `name_expens` =:name_expens, `totalPrice`=:totalPrice, `date`=:date, `location`=:location, `location_name`=:location_name ");
             $stmt->execute([
                 'saller'=>$arrExpenses['saller'],
                 'name_expens'=>$arrExpenses['name_expens'],
                 'totalPrice'=>$arrExpenses['totalPrice'],
-                'date'=>$today,
+                'date'=>$arrExpenses['date'],
+                'location'=>$arrExpenses['location'],
+                'location_name'=>$result['suggestions'][0]['value']
             ]);
 
             return $this->connection->lastInsertId();
@@ -26,6 +51,16 @@ class Report
             file_put_contents('error.txt', $e->getMessage());
         }
 
-
     }
+    public function deleteExpenses($id)
+    {
+        $query = "DELETE FROM `expenses` WHERE `ind` = ?";
+        $params = [$id];
+        $stmt = $this->connection->prepare($query);
+        $stmt->execute($params);
+        return 'Удалено';
+    }
+
+
+
 }
--- botapi/YaDisk/tmebot/devbot.php
+++ botapi/YaDisk/tmebot/devbot.php
@@ -30,6 +30,7 @@ $message_id = $result["message"]["message_id"];
 $text = $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
+$path = $_SERVER['DOCUMENT_ROOT'].'/botapi/YaDisk/tmebot/';
 $keyboard =[
 
     [
@@ -56,28 +57,130 @@ if(isset($callBack))
     {
         $month = explode('|',$callBack);
 
-        file_put_contents('month.txt', $month[1]);
+        if($month[1] == 13)
+        {
+            $date = new DateTime('NOW');
+            $y = $date->format("Y");
+            $m = $date->format("m");
+            $d = $date->format("d");
+            $today = $date->format("Y-m-d");
 
+            file_put_contents('month.txt', $today);
+
+            $keyboard = [
+                'inline_keyboard' =>
+                    [
+                        [
+                            ['text'=> 'Материалы', 'callback_data' => 'type|Материалы'],
+                            ['text'=> 'Аренда', 'callback_data' => 'type|Аренда'],
+
+                        ],
+
+                    ],
+            ];
+            $encodedKeyboard = json_encode($keyboard);
+            $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Выберите тип расходов', 'reply_markup' => $encodedKeyboard]);
+
+
+        }elseif($month[1] == 0){
+
+            $keyboard = [
+                'inline_keyboard' =>
+                    [
+                        [
+                            ['text'=> 'Январь', 'callback_data' => 'month|1'],
+                            ['text'=> 'Февраль', 'callback_data' => 'month|2'],
+                            ['text'=> 'Март', 'callback_data' => 'month|3'],
+                        ],
+                        [
+                            ['text'=> 'Апрель', 'callback_data' => 'month|4'],
+                            ['text'=> 'Май', 'callback_data' => 'month|5'],
+                            ['text'=> 'Июнь', 'callback_data' => 'month|6'],
+                        ],
+                        [
+                            ['text'=> 'Июль', 'callback_data' => 'month|7'],
+                            ['text'=> 'Август', 'callback_data' => 'month|8'],
+                            ['text'=> 'Сентябрь', 'callback_data' => 'month|9'],
+                        ],
+                        [
+                            ['text'=> 'Октябрь', 'callback_data' => 'month|10'],
+                            ['text'=> 'Ноябрь', 'callback_data' => 'month|11'],
+                            ['text'=> 'Декабрь', 'callback_data' => 'month|12'],
+                        ],
+                        [
+                            ['text'=> 'Сегодня', 'callback_data' => 'month|13'],
+                        ]
+                    ],
+            ];
+
+            $encodedKeyboard = json_encode($keyboard);
+
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => 'Выберите месяц', 'reply_markup' => $encodedKeyboard ]);
+
+        }else{
+
+
+            $date = new DateTime('NOW');
+            $y = $date->format("Y");
+            $m = $date->format("m");
+            $d = $date->format("d");
+            $today = $date->format("Y-m-d");
+            $date = $y.'-'.$month[1].'-01';
+            file_put_contents('month.txt', $date);
+
+            $keyboard = [
+                'inline_keyboard' =>
+                    [
+                        [
+                            ['text'=> 'Материалы', 'callback_data' => 'type|Материалы'],
+                            ['text'=> 'Аренда', 'callback_data' => 'type|Аренда'],
+
+                        ],
+
+                    ],
+            ];
+            $encodedKeyboard = json_encode($keyboard);
+            $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Выберите тип расходов', 'reply_markup' => $encodedKeyboard]);
+        }
+
+
+
+    }elseif((strpos($callBack, "pe")>0))
+    {
+        $type = explode('|',$callBack);
+
+        file_put_contents('type.txt', $type[1]);
         $keyboard = [
             'inline_keyboard' =>
                 [
                     [
-                        ['text'=> 'Материалы', 'callback_data' => 'type|Материалы'],
-                        ['text'=> 'Аренда', 'callback_data' => 'type|Аренда'],
+                        ['text'=> 'Да', 'callback_data' => 'locations|yes'],
+                        ['text'=> 'Нет', 'callback_data' => 'locations|no'],
 
                     ],
 
                 ],
         ];
         $encodedKeyboard = json_encode($keyboard);
-
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => 'Выберите тип расходов', 'reply_markup' => $encodedKeyboard ]);
-    }elseif((strpos($callBack, "pe")>0))
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Отправить локацию?', 'reply_markup' => $encodedKeyboard]);
+    }
+    elseif((strpos($callBack, "tio")>0))
     {
-        $type = explode('|',$callBack);
+        $loc = explode('|',$callBack);
+        if($loc[1] === 'no')
+        {
+            file_put_contents('location.txt', '{"latitude":0,"longitude":0}');
 
-        file_put_contents('type.txt', $type[1]);
-        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Сумма расходов']);
+            $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Сумма расходов']);
+        }else{
+            $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Отправь геопозицию']);
+        }
+
+    }elseif ((strpos($callBack, "teExp")) > 0)
+    {
+        $id = explode('|',$callBack);
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $report->deleteExpenses($id)]);
+
     }
 
 
@@ -200,28 +303,10 @@ if($chat_id == $botApiConfiguration->getManagerId() || $botApiConfiguration->get
             'inline_keyboard' =>
                 [
                     [
-                        ['text'=> 'Январь', 'callback_data' => 'month|1'],
-                        ['text'=> 'Февраль', 'callback_data' => 'month|2'],
-                        ['text'=> 'Март', 'callback_data' => 'month|3'],
-                    ],
-                    [
-                        ['text'=> 'Апрель', 'callback_data' => 'month|4'],
-                        ['text'=> 'Май', 'callback_data' => 'month|5'],
-                        ['text'=> 'Июнь', 'callback_data' => 'month|6'],
-                    ],
-                    [
-                        ['text'=> 'Июль', 'callback_data' => 'month|7'],
-                        ['text'=> 'Август', 'callback_data' => 'month|8'],
-                        ['text'=> 'Сентябрь', 'callback_data' => 'month|9'],
-                    ],
-                    [
-                        ['text'=> 'Октябрь', 'callback_data' => 'month|10'],
-                        ['text'=> 'Ноябрь', 'callback_data' => 'month|11'],
-                        ['text'=> 'Декабрь', 'callback_data' => 'month|12'],
-                    ],
-                    [
                         ['text'=> 'Сегодня', 'callback_data' => 'month|13'],
-                    ]
+                        ['text'=> 'Другой месяц', 'callback_data' => 'month|0'],
+
+                    ],
                 ],
         ];
 
@@ -253,16 +338,38 @@ if($chat_id == $botApiConfiguration->getManagerId() || $botApiConfiguration->get
             'name_expens'=>file_get_contents('type.txt'),
             'totalPrice'=>$text,
             'date'=>file_get_contents('month.txt'),
+            'location'=>file_get_contents('location.txt'),
         ];
+
         $res = $report->addExpenses($arrTodb);
         //$res = 1;
 
-        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Расход за месяц - ' .  file_get_contents('month.txt') . ' в категорию - ' . file_get_contents('type.txt') . ' на сумму - ' . $text.' занесен' . 'id-' . $res]);
+        $keyboard = [
+            'inline_keyboard' =>
+                [
+                    [
+                        ['text'=> 'Удалить?', 'callback_data' => 'deleteExpenses|'. $res],
+                    ],
+                ],
+        ];
+
+        $encodedKeyboard = json_encode($keyboard);
+
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Расход за месяц - ' .  file_get_contents('month.txt') . ' в категорию - ' . file_get_contents('type.txt') . ' на сумму - ' . $text.' занесен' . 'id - ' . $res, 'reply_markup' => $encodedKeyboard]);
         file_put_contents('type.txt', 0);
         file_put_contents('month.txt', 0);
+        file_put_contents('location.txt', 0);
     }
+    if(isset($result["message"]["location"]))
+    {
+        file_put_contents('location.txt', $result["message"]["location"]);
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Пришли сумму расхода']);
+    }
+
+
 }else{
 
     $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Только для меня']);
 
 }
+
