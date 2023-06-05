6d45e991d8c36b4a6ad4c3775aaf3c2edd56eb7e
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2023-06-05 21:55:33 +0500

    add scheulder hourly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,10 @@
 
 namespace App\Console;
 
+use App\Models\Visitors;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,6 +18,29 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function (){
+
+            $visitors = DB::table('Visitors')
+                ->select('ip')
+                ->groupBy('ip')
+                ->get();
+
+            $chatId = config('telegram.TELEGRAMADMIN');
+            $token = config('telegram.TELEGRAMTOKEN');
+            $message = 'Всего посетителей: ' . $visitors->count();
+            $response = array(
+                'chat_id' => $chatId,
+                'text' => $message,
+            );
+
+            $ch = curl_init('https://api.telegram.org/bot' . $token . '/sendMessage');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_exec($ch);
+            curl_close($ch);
+        })->hourly();
     }
 
     /**
