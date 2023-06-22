da2bfbd1c792af21996156656edc5f204a0d441b
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2023-06-22 21:17:07 +0500

    add sum date between

--- resources/views/admin/leftMenu.blade.php
+++ resources/views/admin/leftMenu.blade.php
@@ -2,16 +2,6 @@
     <ul class="list-unstyled d-flex justify-content-start card">
         <li class="btn btn-secondary"><a href="/admin/show/all/items/" class="link-light">Продажи</a></li>
         <li class="btn btn-secondary"><a href="/admin/total/year/" class="link-light">Статистика</a></li>
+        <li class="btn btn-secondary"><a href="/admin/sale/date/" class="link-light">Продажи за выбранный период</a></li>
     </ul>
-    <form>
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">@</span>
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-        </div>
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">@</span>
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-        </div>
-        <input type="submit" value="Применить">
-    </form>
 </div>
