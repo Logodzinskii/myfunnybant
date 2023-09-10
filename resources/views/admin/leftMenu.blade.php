25072de111ff0bda5987799ee629ca9f835e4e4c
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2023-09-10 22:23:10 +0500

    add admin max like

--- resources/views/admin/leftMenu.blade.php
+++ resources/views/admin/leftMenu.blade.php
@@ -3,5 +3,6 @@
         <li class="btn btn-secondary"><a href="/admin/show/all/items/" class="link-light">Продажи</a></li>
         <li class="btn btn-secondary"><a href="/admin/total/year/" class="link-light">Статистика</a></li>
         <li class="btn btn-secondary"><a href="/admin/sale/date/" class="link-light">Продажи за выбранный период</a></li>
+        <li class="btn btn-secondary"><a href="/admin/maxlike" class="link-light">Понравились всем</a></li>
     </ul>
 </div>
