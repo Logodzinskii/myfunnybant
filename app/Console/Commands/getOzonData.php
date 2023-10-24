735558d6793ba84fb6a7100cf5f6338b02fea4d8
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2023-10-24 22:02:30 +0500

    edit kernel

---
+++ app/Console/Commands/getOzonData.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+class getOzonData
+{
+    public static function getResponseOzon($data, $method, $request)
+    {
+
+
+        $clientId = config('ozon.CLIENT_ID'); //айди шопа
+        $apiKey = config('ozon.OZONTOKEN');; // ключ апи
+        $url = 'https://api-seller.ozon.ru'.$method;
+        $headers = array(
+            'Content-Type: application/json',
+            'Host: api-seller.ozon.ru',
+            'Client-Id: '.$clientId,
+            'Api-Key: '.$apiKey
+        ) ;
+        $ch = curl_init();
+        $options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => $data,
+            CURLOPT_HTTPHEADER => $headers
+        );
+
+        curl_setopt_array($ch, $options);
+
+        return curl_exec($ch);
+    }
+
+    public static function sendMessageByTelegram(string $message):void
+    {
+        $chatId = config('telegram.TELEGRAMADMIN');
+        $token = config('telegram.TELEGRAMTOKEN');
+
+        $response = array(
+            'chat_id' => $chatId,
+            'text' => $message,
+        );
+
+        $ch = curl_init('https://api.telegram.org/bot' . $token . '/sendMessage');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_exec($ch);
+        curl_close($ch);
+
+    }
+}
