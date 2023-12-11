5c06f61abd2e2ac2ab1badd699c6d166f7585561
Александр Логодзинский <87911051+Logodzinskii@users.noreply.github.com>
2023-12-11 22:06:46 +0500

    Admin db (#39)
    * final v2
    
    * create yml feed
    
    * create yml feed
    
    * create yml feed
    
    * edit admin panel
    
    * add card for to admin panel

--- resources/views/admin/panel/index.blade.php
+++ resources/views/admin/panel/index.blade.php
@@ -2,47 +2,79 @@
 @section('content')
     <section class="container">
         <h2>Панель администратора сайта</h2>
-        <ul>
-            <li>
-                Всего товаров на сайте:
-                <b>{{\App\Models\OzonShopItem::all()->count()}}</b>
-            </li>
-            <li>
-                Последнее обновление товаров на сайте:
-                <b>{{\App\Models\OzonShopItem::query()->select('updated_at')->firstOrFail()->updated_at}}</b>
-            </li>
-            <li>
-                Список пользователей:
-                <ul>
-                    @foreach(\App\Models\User::all() as $email)
-                        <li>имя - {{$email->first_name.$email->name}}, email - {{$email->email}}, создан - {{$email->created_at}} <a href="#">del</a> </li>
-                    @endforeach
-                </ul>
-            </li>
-            <li>
-                Средняя цена товаров на сайте
-                <b>{{\App\Models\StatusPriceShopItems::all()->average('price')}}</b>
-                руб.
-            </li>
-            <li>
-                Средняя цена товаров на сайте с учетом скидок
-                <b>{{\App\Models\StatusPriceShopItems::all()->average('action_price')}}</b>
-                руб.
-            </li>
-            <ul>
-                <li>
+        <div class="container d-flex justify-content-start flex-wrap" >
+            <div class="card">
+                <div class="card-header">
+                    Всего товаров на сайте:
+                </div>
+                <div class="card-body d-flex justify-content-center align-items-center">
+                    <i class="bi-card-list" style="font-size: 1.7em; color: #6610f2"></i>
+                    <b><a href="admin/show/all/products" class="m-3">{{\App\Models\OzonShopItem::all()->count()}}</a></b>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    Последнее обновление товаров на сайте:
+                </div>
+                <div class="card-body d-flex justify-content-center align-items-center">
+                    <i class="bi-arrow-clockwise" style="font-size: 1.7em; color: #6610f2"></i>
+                    <b class="m-3">{{\App\Models\OzonShopItem::query()->select('updated_at')->firstOrFail()->updated_at}}</b>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    Средняя цена товаров на сайте
+                </div>
+                <div class="card-body d-flex justify-content-center align-items-center">
+                    <ul>
+                        <i class="bi-cash-coin" style="font-size: 1.7em; color: #6610f2"></i>
+                        <b class="m-3">{{\App\Models\StatusPriceShopItems::all()->average('price')}}</b>
+                        руб.
+                    </ul>
+                </div>
+            </div>
+            <div class="card ">
+                <div class="card-header">
+                    Средняя цена товаров на сайте с учетом скидок
+                </div>
+                <div class="card-body d-flex justify-content-center align-items-center">
+                    <i class="bi-cash-coin" style="font-size: 1.7em; color: #6610f2"></i>
+                    <b class="m-3">{{\App\Models\StatusPriceShopItems::all()->average('action_price')}}</b>
+                    руб.
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
                     Заказы:
-                    @foreach(\App\Models\UserCart::query()->select('status_offer')->groupBy('status_offer')->get() as $offer)
-                        {{$offer->status_offer}} -
-                        {{\App\Models\UserCart::query()
-                                            ->select('quantity')
-                                            ->where('status_offer','=',$offer->status_offer)
-                                            ->count()}}
+                </div>
+                <div class="card-body d-flex justify-content-center align-items-center flex-wrap">
+                    @foreach(\Illuminate\Support\Facades\DB::table('offer_users')
+                                                                    ->select('status')
+                                                                    ->groupBy('status')
+                                                                    ->get() as $offer)
+                        <div class="m-3">
+                            {{$offer->status}} -
+                            {{\App\Models\OfferUser::query()
+                                            ->select('status')
+                                            ->where('status','=',$offer->status)
+                                            ->count()
+                            }}
+                        </div>
                     @endforeach
-                </li>
-            </ul>
-        </ul>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    Список пользователей:
+                </div>
+                <div class="card-body">
+                    <ul>
+                        @foreach(\App\Models\User::all() as $email)
+                            <li>имя - {{$email->first_name.$email->name}}, email - {{$email->email}}, создан - {{$email->created_at}} <a href="#">del</a> </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
     </section>
-
-
     @endsection
