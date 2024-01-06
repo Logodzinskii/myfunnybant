25176ddd9b3f77d74b97cc072b63bf54fa1fc135
Александр Логодзинский <87911051+Logodzinskii@users.noreply.github.com>
2024-01-06 22:30:17 +0500

    Admin db (#46)
    * final v2
    
    * create yml feed
    
    * create yml feed
    
    * create yml feed
    
    * edit admin panel
    
    * add card for to admin panel
    
    * add card for to admin panel 1
    
    * create ozon finance report
    
    * create charts for sales and ozon
    
    * create charts for sales and ozon
    
    * create edit date sale in sale_items
    
    * create privacy
    
    * edit main index page
    
    * edit main index page
    
    * create component card for item
    
    * edit cart
    
    * edit pagination
    
    * edit find pagination
    
    * edit funnel
    
    * create filter colors
    
    * create filter colors and materials
    
    * create filter price
    
    * edit for mobile funnel

--- resources/views/components/funnel.blade.php
+++ resources/views/components/funnel.blade.php
@@ -12,30 +12,35 @@
             </a>
         </form>
     </div>
-    <div class="dropdown sticky-top">
-        <form class="d-flex" method="post" action="{{url('/filter/78059066')}}">
-            @csrf
-            <div class="nav-item dropdown m-3">
-                <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="bi-funnel-fill rounded-2 text-light m-2 p-2" style="background-color: #6610f2; size: 2em"></i>По категории
-                </a>
-                <ul class="dropdown-menu">
-                    @foreach(\Illuminate\Support\Facades\DB::table('ozon_shop_items')->orderBy('created_at', 'desc')->groupBy('category')->get() as $category)
-                    <li>
-                        <a class="dropdown-item" href="{{url('/filter/'.$category->category)}}">{{$category->type}}</a>
-                    </li>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="nav-item dropdown m-3">
-                <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="bi-funnel-fill rounded-2 text-light m-2 p-2" style="background-color: #6610f2; size: 2em"></i>По цвету
-                </a>
-                <ul class="dropdown-menu overflow-scroll" style="height: 80vh">
-                    @php
-                        $newArr = [];
-                    @endphp
-                    @foreach(\Illuminate\Support\Facades\DB::table('ozon_shop_items')->orderBy('colors', 'desc')->groupBy('colors')->get() as $colors)
+    <p>
+        <a class="btn rounded-2 text-light m-2 p-2" style="background-color: #6610f2; size: 2em" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Фильтры
+        </a>
+    </p>
+    <div class="collapse" id="collapseExample">
+        <div class="card card-body">
+            <div class="dropdown sticky-top d-flex justify-content-start flex-wrap">
+                <div class="nav-item dropdown m-3">
+                    <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi-funnel-fill rounded-2 text-light m-2 p-2" style="background-color: #6610f2; size: 2em"></i>По категории
+                    </a>
+                    <ul class="dropdown-menu">
+                        @foreach(\Illuminate\Support\Facades\DB::table('ozon_shop_items')->orderBy('created_at', 'desc')->groupBy('category')->get() as $category)
+                            <li>
+                                <a class="dropdown-item" href="{{url('/filter/'.$category->category)}}">{{$category->type}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="nav-item dropdown m-3">
+                    <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi-funnel-fill rounded-2 text-light m-2 p-2" style="background-color: #6610f2; size: 2em"></i>По цвету
+                    </a>
+                    <ul class="dropdown-menu overflow-scroll" style="height: 80vh">
+                        @php
+                            $newArr = [];
+                        @endphp
+                        @foreach(\Illuminate\Support\Facades\DB::table('ozon_shop_items')->orderBy('colors', 'desc')->groupBy('colors')->get() as $colors)
                             @php
                                 $arr = json_decode($colors->colors,true);
 
@@ -45,54 +50,55 @@
                                                 $newArr[] = implode(' ', $string);
                                             }
                             @endphp
-                    @endforeach
-                    @foreach(array_unique($newArr) as $color)
-                        <li>
-                            <a class="dropdown-item" href="{{url('/filter/colors/'.$color)}}">{{$color}}</a>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="nav-item dropdown m-3">
-                <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="bi-funnel-fill rounded-2 text-light m-2 p-2" style="background-color: #6610f2; size: 2em"></i>По материалу
-                </a>
-                <ul class="dropdown-menu overflow-scroll" style="height: 80vh">
-                    @php
-                        $newArr = [];
-                    @endphp
-                    @foreach(\Illuminate\Support\Facades\DB::table('ozon_shop_items')->orderBy('material', 'desc')->groupBy('material')->get() as $colors)
+                        @endforeach
+                        @foreach(array_unique($newArr) as $color)
+                            <li>
+                                <a class="dropdown-item" href="{{url('/filter/colors/'.$color)}}">{{$color}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="nav-item dropdown m-3">
+                    <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi-funnel-fill rounded-2 text-light m-2 p-2" style="background-color: #6610f2; size: 2em"></i>По материалу
+                    </a>
+                    <ul class="dropdown-menu overflow-scroll" style="height: 80vh">
                         @php
-                            $arr = json_decode($colors->material,true);
-
-                                    foreach ($arr as $arrString)
-                                        {
-                                            $string = explode(' ', $arrString);
-                                            $newArr[] = implode(' ', $string);
-                                        }
+                            $newArr = [];
                         @endphp
-                    @endforeach
-                    @foreach(array_unique($newArr) as $color)
-                        <li>
-                            <a class="dropdown-item" href="{{url('/filter/material/'.$color)}}">{{$color}}</a>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="nav-item dropdown m-3">
-                <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="bi-funnel-fill rounded-2 text-light m-2 p-2" style="background-color: #6610f2; size: 2em"></i>По цене
-                </a>
-                <ul class="dropdown-menu">
+                        @foreach(\Illuminate\Support\Facades\DB::table('ozon_shop_items')->orderBy('material', 'desc')->groupBy('material')->get() as $colors)
+                            @php
+                                $arr = json_decode($colors->material,true);
+
+                                        foreach ($arr as $arrString)
+                                            {
+                                                $string = explode(' ', $arrString);
+                                                $newArr[] = implode(' ', $string);
+                                            }
+                            @endphp
+                        @endforeach
+                        @foreach(array_unique($newArr) as $color)
+                            <li>
+                                <a class="dropdown-item" href="{{url('/filter/material/'.$color)}}">{{$color}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="nav-item dropdown m-3">
+                    <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi-funnel-fill rounded-2 text-light m-2 p-2" style="background-color: #6610f2; size: 2em"></i>По цене
+                    </a>
+                    <ul class="dropdown-menu">
                         <li>
                             <a class="dropdown-item" href="{{url('/filter/price/max')}}">Сначала дорогие</a>
                         </li>
-                    <li>
-                        <a class="dropdown-item" href="{{url('/filter/price/min')}}">Сначала недорогие</a>
-                    </li>
-                </ul>
+                        <li>
+                            <a class="dropdown-item" href="{{url('/filter/price/min')}}">Сначала недорогие</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </form>
+        </div>
     </div>
 </div>
 
